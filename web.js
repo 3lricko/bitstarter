9a0814cd20aaef053d0430a1aa08007e2bde902b
Rick Glez <elricko@gmail.com>
2013-07-16 04:11:23 +0000

    web.js mistyped

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var fs = requiere('fs');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    
+
     response.send(fs.readFileSync('index.html').toString());
 });
 
